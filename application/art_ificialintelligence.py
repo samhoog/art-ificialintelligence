7417d1d8ce77a635d03cee46d6ac268af42814e7
samhoog <samuel.hoogewind@icloud.com>
2023-04-19 18:09:14 -0400

    application done

--- application/art_ificialintelligence.py
+++ application/art_ificialintelligence.py
@@ -1,21 +1,22 @@
-
+from fastai.vision.all import *
 import numpy as np
-import cairocffi as cairo
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
 from PIL import Image
-import doodle_classification
 
-# Specify canvas parameters in application
-drawing_mode = st.sidebar.selectbox(
-    "Drawing tool:", ("freedraw", "line", "rect", "circle", "transform")
-)
+import pathlib
+temp = pathlib.PosixPath
+pathlib.PosixPath = pathlib.WindowsPath
 
-stroke_width = st.sidebar.slider("Stroke width: ", 1, 25, 3)
+classifier = load_learner('doodle_classifier.pkl')
+
+stroke_width = st.sidebar.slider("Stroke width: ", 10, 25, 15)
 
 realtime_update = st.sidebar.checkbox("Update in realtime", True)
 
-
+categories = ['bicycle', 'cactus', 'camera', 'flower', 'helicopter', 'monkey', 'moon',
+              'mug', 'octopus', 'owl', 'panda', 'shorts', 'strawberry', 'submarine',
+              'sword', 'telephone', 'tree', 'truck', 'violin', 'windmill']
 
 # Create a canvas component
 canvas_result = st_canvas(
@@ -24,14 +25,30 @@ canvas_result = st_canvas(
     stroke_color='white',
     background_color='black',
     update_streamlit=realtime_update,
-    height=500,
-    width=500,
-    drawing_mode=drawing_mode,
+    height=448,
+    width=448,
+    drawing_mode="freedraw",
     key="canvas",
 )
 
-# Do something interesting with the image data and paths
+# Convert the image to a 28 x 28 numpy
 img = Image.fromarray(canvas_result.image_data)
-img = img.resize(size=(28, 28))
-st.image(img)
+img = img.resize(size=(28, 28)).convert('RGB')
+img = np.array(img)
 
+# Predict what the image is
+prediction = classifier.predict(img)
+
+# Calculate the guess and highest percentage and put them on the screen
+percentages = prediction[2].numpy()
+max_percentage = percentages.max()
+max_percentage = round(max_percentage * 100, 3)
+st.write('I think it is a ' + str(prediction[0]))
+st.write('I am ' + str(max_percentage) + '% confident!')
+
+# Calculate the percentages and put them on the sidebar
+percentages = (percentages * 100).tolist()
+percentages = [round(percentage, 3) for percentage in percentages]
+res = {categories[i]: percentages[i] for i in range(len(categories))}
+with st.sidebar:
+    st.write(res)
