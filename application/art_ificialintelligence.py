e54d946dea015b36c8656e34fb5b657e7233284b
samhoog <samuel.hoogewind@icloud.com>
2023-04-20 11:34:49 -0400

    test

--- application/art_ificialintelligence.py
+++ application/art_ificialintelligence.py
@@ -9,7 +9,7 @@ temp = pathlib.PosixPath
 pathlib.PosixPath = pathlib.WindowsPath
 
 st.title("Art-ificial Intelligence")
-
+st.write(os.getcwd())
 classifier = load_learner('doodle_classifier.pkl')
 
 stroke_width = st.sidebar.slider("Stroke width: ", 10, 25, 15)
